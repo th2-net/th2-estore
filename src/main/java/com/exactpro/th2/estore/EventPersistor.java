118084e5f87f0498433ce2cadc2f874f2e141bdf
georgiano <15014186+georgiano@users.noreply.github.com>
2022-09-02 17:32:40 +0400

    [TH2-4111] Added metrics collection for exporting to Prometheus

--- src/main/java/com/exactpro/th2/estore/EventPersistor.java
+++ src/main/java/com/exactpro/th2/estore/EventPersistor.java
@@ -22,12 +22,16 @@ import com.exactpro.th2.taskutils.BlockingScheduledRetryableTaskQueue;
 import com.exactpro.th2.taskutils.FutureTracker;
 import com.exactpro.th2.taskutils.RetryScheduler;
 import com.exactpro.th2.taskutils.ScheduledRetryableTask;
+import io.prometheus.client.Histogram;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.requireNonNull;
 
@@ -43,6 +47,9 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent> {
     private final Object signal = new Object();
     private final int maxTaskRetries;
 
+    private final EventPersistorMetrics metrics;
+    private final ScheduledExecutorService samplerService;
+
     public EventPersistor(@NotNull Configuration config, @NotNull CradleManager cradleManager) {
         this(config, cradleManager, (r) -> config.getRetryDelayBase() * 1_000_000 * (r + 1));
     }
@@ -52,7 +59,10 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent> {
         this.cradleStorage = requireNonNull(cradleManager.getStorage(), "Cradle storage can't be null");
         this.taskQueue = new BlockingScheduledRetryableTaskQueue<>(config.getMaxTaskCount(), config.getMaxTaskDataSize(), scheduler);
         this.futures = new FutureTracker<>();
+        this.metrics = new EventPersistorMetrics(taskQueue);
+        this.samplerService = Executors.newSingleThreadScheduledExecutor();
     }
+
 
     public void start() throws InterruptedException {
         this.stopped = false;
@@ -71,6 +81,12 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent> {
 
         LOGGER.info("EventProcessor started. Maximum data size for tasks = {}, maximum number of tasks = {}",
                 taskQueue.getMaxDataSize(), taskQueue.getMaxTaskCount());
+        samplerService.scheduleWithFixedDelay(
+                () -> metrics.takeQueueMeasurements(),
+                0,
+                1,
+                TimeUnit.SECONDS
+        );
         while (!stopped) {
             try {
                 ScheduledRetryableTask<StoredTestEvent> task = taskQueue.awaitScheduled();
@@ -89,15 +105,27 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent> {
 
     @Override
     public void persist(StoredTestEvent event) {
-        final long size;
-        if (event instanceof StoredTestEventSingle) {
-            size = ((StoredTestEventSingle) event).getContent().length;
-        } else if (event instanceof StoredTestEventBatch) {
-            size = ((StoredTestEventBatch) event).getBatchSize();
-        } else
-            throw new IllegalArgumentException(String.format("Unknown data class (%s)", event.getClass().getSimpleName()));
+        metrics.takeQueueMeasurements();
+        taskQueue.submit(new ScheduledRetryableTask<>(System.nanoTime(), maxTaskRetries, getEventContentSize(event), event));
+    }
 
-        taskQueue.submit(new ScheduledRetryableTask<>(System.nanoTime(), maxTaskRetries, size, event));
+
+    public long getEventContentSize(StoredTestEvent event) {
+        if (event instanceof StoredTestEventSingle)
+            return ((StoredTestEventSingle) event).getContent().length;
+        else if (event instanceof StoredTestEventBatch)
+            return ((StoredTestEventBatch) event).getBatchSize();
+        else
+            throw new IllegalArgumentException(String.format("Unknown event class (%s)", event.getClass().getSimpleName()));
+    }
+
+    private int getEventCount(StoredTestEvent event) {
+        if (event instanceof StoredTestEventSingle)
+            return 1;
+        else if (event instanceof StoredTestEventBatch)
+            return ((StoredTestEventBatch) event).getTestEventsCount();
+        else
+            throw new IllegalArgumentException(String.format("Unknown event class (%s)", event.getClass().getSimpleName()));
     }
 
 
@@ -111,20 +139,29 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent> {
         } catch (Exception ex) {
             LOGGER.error("Cannot await all futures are finished", ex);
         }
+        try {
+            samplerService.shutdown();
+            samplerService.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+        }
     }
 
 
     void processTask(ScheduledRetryableTask<StoredTestEvent> task) throws IOException {
 
-        StoredTestEvent event = task.getPayload();
+        final StoredTestEvent event = task.getPayload();
+        final Histogram.Timer timer = metrics.startMeasuringPersistenceLatency();
         CompletableFuture<Void> result = cradleStorage.storeTestEventAsync(event)
                 .thenRun(() -> LOGGER.debug("Stored batch id '{}' parent id '{}'", event.getId(), event.getParentId()))
                 .whenCompleteAsync((unused, ex) ->
                 {
+                    timer.observeDuration();
                     if (ex != null)
                         logAndRetry(task, ex);
-                    else
+                    else {
                         taskQueue.complete(task);
+                        metrics.updateEventMeasurements(getEventCount(event), task.getPayloadSize());
+                    }
                 }
                 );
 
@@ -133,18 +170,28 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent> {
 
 
     private void logAndRetry(ScheduledRetryableTask<StoredTestEvent> task, Throwable e) {
+
+        metrics.registerPersistenceFailure();
+        int retriesDone = task.getRetriesDone() + 1;
+
         if (task.getRetriesLeft() > 0) {
+
             LOGGER.error("Failed to store the event batch id '{}', {} retries left, rescheduling",
                     task.getPayload().getId(),
                     task.getRetriesLeft(),
                     e);
             taskQueue.retry(task);
+            metrics.registerPersistenceRetry(retriesDone);
+
         } else {
+
             taskQueue.complete(task);
+            metrics.registerAbortedPersistence();
             LOGGER.error("Failed to store the event batch id '{}', aborting after {} executions",
                     task.getPayload().getId(),
-                    task.getRetriesDone() + 1,
+                    retriesDone,
                     e);
+
         }
     }
 }
---
+++ src/main/java/com/exactpro/th2/estore/EventPersistorMetrics.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2020-2022 Exactpro (Exactpro Systems Limited)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exactpro.th2.estore;
+
+import com.exactpro.th2.taskutils.BlockingScheduledRetryableTaskQueue;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.Histogram;
+
+public class EventPersistorMetrics {
+    private static final Gauge GAUGE_QUEUE_TASK_CAPACITY = Gauge
+            .build("persistor_queue_task_capacity", "Events queue size").register();
+
+    private static final Gauge GAUGE_QUEUE_USED_TASK_COUNT = Gauge
+            .build("persistor_queue_task_count", "Number of events queued for persistence").register();
+
+    private static final Gauge GAUGE_QUEUE_FREE_TASK_COUNT = Gauge
+            .build("persistor_queue_free_tasks", "Number of events that can be queued").register();
+
+    private static final Gauge GAUGE_QUEUE_MAX_DATA_SIZE = Gauge
+            .build("persistor_queue_max_data_size", "Max data size that can be queued").register();
+
+    private static final Gauge GAUGE_QUEUE_USED_DATA_SIZE = Gauge
+            .build("persistor_queue_used_data_size", "Data size of queued events").register();
+
+    private static final Gauge GAUGE_QUEUE_FREE_DATA_SIZE = Gauge
+            .build("persistor_queue_free_data_size", "Available data size").register();
+
+    private static final Counter COUNTER_EVENTS_PERSISTED = Counter
+            .build("persistor_events_persisted", "Number of events persisted").register();
+
+    private static final Counter COUNTER_EVENTS_SIZE_PERSISTED = Counter
+            .build("persistor_events_sizes_persisted", "Content size of events that persisted").register();
+
+    private static final Counter COUNTER_PERSISTENCE_FAILURES = Counter
+            .build("persistor_persistence_failures", "Number of event persistence failures").register();
+
+    private static final Counter COUNTER_ABORTED_PERSISTENCES = Counter
+            .build("persistor_aborted_persistences", "Number of aborted event persistences").register();
+
+    private static final Counter COUNTER_1ST_RETRIES = Counter
+            .build("persistor_1st_retries", "Number of primary persistence retries").register();
+
+    private static final Counter COUNTER_2ND_RETRIES = Counter
+            .build("persistor_2nd_retries", "Number of secondary persistence retries").register();
+
+    private static final Counter COUNTER_FURTHER_RETRIES = Counter
+            .build("persistor_further_retries", "Number of further persistence retries").register();
+
+    private static final Histogram HISTOGRAM_PERSISTENCE_LATENCY = Histogram
+            .build("persistor_persistence_latency", "Event persistence latency")
+            .buckets(0.010, 0.020, 0.050, 0.100, 0.200, 0.300, 0.400, 0.500, 1.000, 1.500, 2.000, 2.500, 3.000, 4.000, 5.000, 10.000)
+            .register();
+
+    private final BlockingScheduledRetryableTaskQueue taskQueue;
+
+    public EventPersistorMetrics(BlockingScheduledRetryableTaskQueue taskQueue) {
+        this.taskQueue = taskQueue;
+
+    }
+
+    public void takeQueueMeasurements() {
+
+        int usedTasks = taskQueue.getTaskCount();
+        int freeTasks = taskQueue.getMaxTaskCount() - usedTasks;
+
+        GAUGE_QUEUE_TASK_CAPACITY.set(taskQueue.getMaxTaskCount());
+        GAUGE_QUEUE_USED_TASK_COUNT.set(usedTasks);
+        GAUGE_QUEUE_FREE_TASK_COUNT.set(freeTasks);
+
+        long maxDataSize = taskQueue.getMaxDataSize();
+        long usedDataSize = taskQueue.getUsedDataSize();
+        long freeDataSize = maxDataSize - usedDataSize;
+
+        GAUGE_QUEUE_MAX_DATA_SIZE.set(maxDataSize);
+        GAUGE_QUEUE_USED_DATA_SIZE.set(usedDataSize);
+        GAUGE_QUEUE_FREE_DATA_SIZE.set(freeDataSize);
+    }
+
+
+    public void updateEventMeasurements(int events, long eventSizes) {
+
+        COUNTER_EVENTS_PERSISTED.inc(events);
+        COUNTER_EVENTS_SIZE_PERSISTED.inc(eventSizes);
+    }
+
+
+    public Histogram.Timer startMeasuringPersistenceLatency() {
+
+        return HISTOGRAM_PERSISTENCE_LATENCY.startTimer();
+    }
+
+
+    public void registerPersistenceFailure() {
+        COUNTER_PERSISTENCE_FAILURES.inc();
+    }
+
+
+    public void registerAbortedPersistence() {
+        COUNTER_ABORTED_PERSISTENCES.inc();
+    }
+
+
+    public void registerPersistenceRetry(int retryNumber) {
+        switch (retryNumber) {
+            case 1:
+                COUNTER_1ST_RETRIES.inc();
+                break;
+            case 2:
+                COUNTER_2ND_RETRIES.inc();
+                break;
+            default:
+                if (retryNumber < 1)
+                    throw new IllegalArgumentException("Invalid value : " + retryNumber);
+                else
+                    COUNTER_FURTHER_RETRIES.inc();
+        }
+    }
+}
