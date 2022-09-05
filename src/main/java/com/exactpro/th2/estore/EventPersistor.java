381c7d10f518cbf4c0d90e8226998759b2b68e00
giorgi.mania <giorgi.mania@exactprosystems.com>
2022-09-05 14:27:13 +0400

    [TH2-4177] ported metrics collection code from dev-version-4

--- src/main/java/com/exactpro/th2/estore/EventPersistor.java
+++ src/main/java/com/exactpro/th2/estore/EventPersistor.java
@@ -16,17 +16,22 @@ package com.exactpro.th2.estore;
 import com.exactpro.cradle.CradleStorage;
 import com.exactpro.cradle.testevents.StoredTestEvent;
 import com.exactpro.cradle.testevents.StoredTestEventBatch;
+import com.exactpro.cradle.testevents.StoredTestEventSingle;
 import com.exactpro.cradle.testevents.StoredTestEventWithContent;
 import com.exactpro.th2.taskutils.BlockingScheduledRetryableTaskQueue;
 import com.exactpro.th2.taskutils.FutureTracker;
 import com.exactpro.th2.taskutils.RetryScheduler;
 import com.exactpro.th2.taskutils.ScheduledRetryableTask;
+import io.prometheus.client.Histogram;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.requireNonNull;
 
@@ -42,6 +47,9 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent>, Aut
     private final Object signal = new Object();
     private final int maxTaskRetries;
 
+    private final EventPersistorMetrics metrics;
+    private final ScheduledExecutorService samplerService;
+
     public EventPersistor(@NotNull Configuration config, @NotNull CradleStorage cradleStorage) {
         this(config, cradleStorage, (r) -> config.getRetryDelayBase() * 1_000_000 * (r + 1));
     }
@@ -51,7 +59,10 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent>, Aut
         this.cradleStorage = requireNonNull(cradleStorager, "Cradle storage can't be null");
         this.taskQueue = new BlockingScheduledRetryableTaskQueue<>(config.getMaxTaskCount(), config.getMaxTaskDataSize(), scheduler);
         this.futures = new FutureTracker<>();
+        this.metrics = new EventPersistorMetrics(taskQueue);
+        this.samplerService = Executors.newSingleThreadScheduledExecutor();
     }
+
 
     public void start() throws InterruptedException {
         this.stopped = false;
@@ -70,6 +81,12 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent>, Aut
 
         LOGGER.info("EventProcessor started. Maximum data size for tasks = {}, maximum number of tasks = {}",
                 taskQueue.getMaxDataSize(), taskQueue.getMaxTaskCount());
+        samplerService.scheduleWithFixedDelay(
+                () -> metrics.takeQueueMeasurements(),
+                0,
+                1,
+                TimeUnit.SECONDS
+        );
         while (!stopped) {
             try {
                 ScheduledRetryableTask<StoredTestEvent> task = taskQueue.awaitScheduled();
@@ -88,17 +105,28 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent>, Aut
 
     @Override
     public void persist(StoredTestEvent event) {
-        final long size;
-        if (event instanceof StoredTestEventWithContent) {
-            size = ((StoredTestEventWithContent) event).getContent().length;
-        } else if (event instanceof StoredTestEventBatch) {
-            size = ((StoredTestEventBatch) event).getBatchSize();
-        } else
-            throw new IllegalArgumentException(String.format("Unknown data class (%s)", event.getClass().getSimpleName()));
-
-        taskQueue.submit(new ScheduledRetryableTask<>(System.nanoTime(), maxTaskRetries, size, event));
+        metrics.takeQueueMeasurements();
+        taskQueue.submit(new ScheduledRetryableTask<>(System.nanoTime(), maxTaskRetries, getEventContentSize(event), event));
     }
 
+
+    public long getEventContentSize(StoredTestEvent event) {
+        if (event instanceof StoredTestEventWithContent)
+            return ((StoredTestEventWithContent) event).getContent().length;
+        else if (event instanceof StoredTestEventBatch)
+            return ((StoredTestEventBatch) event).getBatchSize();
+        else
+            throw new IllegalArgumentException(String.format("Unknown event class (%s)", event.getClass().getSimpleName()));
+    }
+
+    private int getEventCount(StoredTestEvent event) {
+        if (event instanceof StoredTestEventSingle)
+            return 1;
+        else if (event instanceof StoredTestEventBatch)
+            return ((StoredTestEventBatch) event).getTestEventsCount();
+        else
+            throw new IllegalArgumentException(String.format("Unknown event class (%s)", event.getClass().getSimpleName()));
+    }
 
     public void close () {
 
@@ -110,21 +138,30 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent>, Aut
         } catch (Exception ex) {
             LOGGER.error("Cannot await all futures are finished", ex);
         }
+        try {
+            samplerService.shutdown();
+            samplerService.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+        }
     }
 
 
     void processTask(ScheduledRetryableTask<StoredTestEvent> task) throws IOException {
 
-        StoredTestEvent event = task.getPayload();
+        final StoredTestEvent event = task.getPayload();
+        final Histogram.Timer timer = metrics.startMeasuringPersistenceLatency();
         CompletableFuture<Void> result = cradleStorage.storeTestEventAsync(event)
                 .thenRun(() -> LOGGER.debug("Stored batch id '{}' parent id '{}'", event.getId(), event.getParentId()))
                 .whenCompleteAsync((unused, ex) ->
-                        {
-                            if (ex != null)
-                                logAndRetry(task, ex);
-                            else
-                                taskQueue.complete(task);
-                        }
+                {
+                    timer.observeDuration();
+                    if (ex != null)
+                        logAndRetry(task, ex);
+                    else {
+                        taskQueue.complete(task);
+                        metrics.updateEventMeasurements(getEventCount(event), task.getPayloadSize());
+                    }
+                }
                 );
 
         futures.track(result);
@@ -132,18 +169,28 @@ public class EventPersistor implements Runnable, Persistor<StoredTestEvent>, Aut
 
 
     private void logAndRetry(ScheduledRetryableTask<StoredTestEvent> task, Throwable e) {
+
+        metrics.registerPersistenceFailure();
+        int retriesDone = task.getRetriesDone() + 1;
+
         if (task.getRetriesLeft() > 0) {
+
             LOGGER.error("Failed to store the event batch id '{}', {} retries left, rescheduling",
                     task.getPayload().getId(),
                     task.getRetriesLeft(),
                     e);
             taskQueue.retry(task);
+            metrics.registerPersistenceRetry(retriesDone);
+
         } else {
+
             taskQueue.complete(task);
+            metrics.registerAbortedPersistence();
             LOGGER.error("Failed to store the event batch id '{}', aborting after {} executions",
                     task.getPayload().getId(),
-                    task.getRetriesDone() + 1,
+                    retriesDone,
                     e);
+
         }
     }
 }
--- src/main/java/com/exactpro/th2/estore/EventProcessor.java
+++ src/main/java/com/exactpro/th2/estore/EventProcessor.java
@@ -31,9 +31,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import static com.exactpro.th2.estore.ProtoUtil.toCradleEvent;
 import static com.exactpro.th2.estore.ProtoUtil.toCradleEventID;
@@ -46,8 +43,6 @@ public class EventProcessor {
     private final MessageRouter<EventBatch> router;
     private final CradleObjectsFactory objectsFactory;
     private final Persistor<StoredTestEvent> persistor;
-    private final Metrics performanceMetrics;
-    private final ScheduledExecutorService performanceLoggerService;
     private SubscriberMonitor monitor;
 
     public EventProcessor(@NotNull MessageRouter<EventBatch> router,
@@ -56,24 +51,14 @@ public class EventProcessor {
         this.router = requireNonNull(router, "Message router can't be null");
         this.objectsFactory = requireNonNull(objectsFactory, "Cradle entities factory can't be null");
         this.persistor = persistor;
-        this.performanceMetrics = new Metrics();
-        this.performanceLoggerService = Executors.newSingleThreadScheduledExecutor();
     }
 
     public void start() {
-        performanceLoggerService.scheduleWithFixedDelay(
-                () -> LOGGER.info("Queue processing speed is {} batches per second"
-                        , String.format("%.02f", performanceMetrics.getCurrentSpeed())),
-                0,
-                1,
-                TimeUnit.SECONDS
-        );
 
         if (monitor == null) {
             monitor = router.subscribeAll((tag, delivery) -> {
                 try {
                     handle(delivery);
-                    performanceMetrics.addValue(1);
                 } catch (Exception e) {
                     LOGGER.warn("Cannot handle delivery from consumer = {}", tag, e);
                 }
@@ -85,6 +70,7 @@ public class EventProcessor {
             LOGGER.info("RabbitMQ subscribing was successful");
         }
     }
+
 
     public void handle(EventBatch eventBatch) {
         try {
@@ -101,7 +87,6 @@ public class EventProcessor {
             else
                 for (Event event : events)
                     storeSingleEvent(event);
-
         } catch (Exception e) {
             LOGGER.error("Failed to store event batch '{}'", TextFormat.shortDebugString(eventBatch), e);
             throw new RuntimeException("Failed to store event batch", e);
@@ -116,11 +101,6 @@ public class EventProcessor {
             } catch (Exception e) {
                 LOGGER.error("Cannot unsubscribe from queues", e);
             }
-        }
-        try {
-            performanceLoggerService.shutdown();
-            performanceLoggerService.awaitTermination(1, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
         }
     }
 
